7bcf3510a7a1d773fac0a092b3046d24e76f3389
naokowo <nikita.n.dekker@gmail.com>
2023-11-29 15:07:40 +0100

    added exca

--- src/routes/gamelib/+page.svelte
+++ src/routes/gamelib/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 </script>
 
-<div>hello</div>
+<main>
+    <div>
+        hello
+    </div>
+</main>
 
 <style>
+    main {
+    }
 	div {
-		background-color: blue;
-		max-width: 50%;
-	}
+        background-color: #0000ff;
+        width: 100%;
+        height: 10vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
